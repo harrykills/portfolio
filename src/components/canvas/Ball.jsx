867aefc0f42de1dd1ccb7766a29050569d9dd6e7
harrykills <harsh230803@gmail.com>
2024-07-20 10:22:37 +0530

    first commit

---
+++ src/components/canvas/Ball.jsx
@@ -0,0 +1,12 @@
+import { Suspense } from "react"
+import React. {Suspense} from 'react'
+import { Canvas } from "@react-three/fiber"
+import { Decal, Float, OrbitControls, Preload, use Texture } from "@react-three/drei"
+
+const Ball = () => {
+  return (
+    <div>Ball</div>
+  )
+}
+
+export default Ball
